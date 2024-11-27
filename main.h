d0b4a8572ea71305364c42e69c941774fc08a8c9
blasey <justblasey@gmail.com>
2024-11-27 14:36:52 -0400

    Adding file with prototypes

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef main_h
+#define main_h
+
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * _printf - custom print function
+ * @format: the format string containing the specifiers
+ *
+ * Return: the number of characters printed (excluding the null byte used to end output to strings)
+ */
+
+int _printf(const char *format, ...)
+
+#endif
