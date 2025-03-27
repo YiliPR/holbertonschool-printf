601d4d656756bee7005701803701b29ff34760f7
yilipr <elijfigueroaperez@egmail.com>
2025-03-27 13:29:29 -0400

    created draft of main function file

--- _printf.c
+++ _printf.c
@@ -1,1 +1,44 @@
 #include "main.h"
+
+/**
+ * _printf - custom printf function made by us.
+ *
+ * @format: constant character pointer.
+ *
+ * Return: Characters printed.
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int i = 0, count = 0;
+
+	if (!format)
+		return (-1);
+
+	va_start(args, format);
+
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%' && format[i + 1] != '\0')
+		{
+			i++;
+			if (format[i] == 'c')
+				(va_arg(args, int));
+			else if (format[i] == 's')
+				(va_arg(args, char *));
+			else if (format[i] == 'd' || format[i] == 'i')
+				(va_arg(args, int));
+			else
+			{
+				count += ;
+				count += ;
+			}
+		}
+		else
+			count +=
+		i++;
+	}
+	va_end;
+	return (count);
+}
